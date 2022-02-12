9b1edd47327c883053e97abaca0f2a8173f2a5f1
Gyosic <zpzptls@naver.com>
2022-02-12 16:57:55 +0900

    Add get / response

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,10 @@ const { chart, position, status } = require("../data/data");
 const express = require("express");
 const router = express.Router();
 
+router.get("/", function (res, req, next) {
+  res.send("Hello World! Smart City System Server!");
+});
+
 router.get("/api", function (req, res, next) {
   res.send({ dataList: { chart, position, status } });
 });
